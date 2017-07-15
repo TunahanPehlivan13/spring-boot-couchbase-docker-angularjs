bb3614646820d5418aaa765eb341e7972adfd7c9
tunahan.pehlivan <tunahan.pehlivan@n11.com>
2017-07-15 19:43:22 +0300

    trivial changes

--- couchbase/run-couchbase.sh
+++ couchbase/run-couchbase.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker build -t todo/couchbase .
-docker run -d --name couchbase -p 8091-8093:8091-8093 -p 11210:11210 todo/couchbase
+docker run -d -p 8091-8093:8091-8093 -p 11210:11210 todo/couchbase
