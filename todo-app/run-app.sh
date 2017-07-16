2cf679d500bd4522f1805e389775b727defe4145
tunahan.pehlivan <tunahan.pehlivan@n11.com>
2017-07-16 16:20:26 +0300

    docker image name changes

--- todo-app/run-app.sh
+++ todo-app/run-app.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 mvn clean package -DskipTests
 
-docker build -t todo/app .
-docker run -p 8080:8080 -t todo/app
+docker build -t tunahanpehlivan/todo-app .
+docker run -p 8080:8080 -t tunahanpehlivan/todo-app
